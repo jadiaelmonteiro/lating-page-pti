d86e4227d6f16f1d86035f8ccb6152da4010c229
jadiaelmonteiro <jadiael.monteiro@gmail.com>
2023-05-13 10:33:43 -0300

    Add new features

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 function send()
-{
-    alert('Dados enviados!');
+{  
+    let name = document.querySelector('#name').value;
+    let phone = document.querySelector('#phone').value;
+
+    if (name != '' && phone != '') {
+        alert('Dados enviados com sucesso!');
+    } else {
+        alert('Por favor, preencha os campos com *');
+    }
 }
