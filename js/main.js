32b904d36835e06acfc5b59819b155fa478d36b9
verdanatech-jadiael <jadiael.monteiro@verdanatech.com>
2023-05-13 14:06:23 -0300

    Refactor

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-function send()
-{  
-    let name = document.querySelector('#name').value;
-    let phone = document.querySelector('#phone').value;
-
-    if (name != '' && phone != '') {
-        alert('Dados enviados com sucesso!');
-    } else {
-        alert('Por favor, preencha os campos com *');
-    }
+function send() {
+    alert('Dados enviados com sucesso!');
 }
